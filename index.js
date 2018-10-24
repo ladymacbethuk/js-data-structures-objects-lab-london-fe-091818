5fa3c3403db6696d44401a0801cb1679994b63bf
Stephanie Phillips <steph@ladymacbeth.uk>
2018-10-24 19:42:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 function deleteFromDriverByKey(driver, key) {
   console.log("the argument called driver has a value of: ", driver);
   console.log("the argument called key has a value of: ", key );
-  const newdriver = Object.assign ({}, driver);
+  const newdriver = Object.assign ({}, key, value);
   console.log("newdriver is : ", newdriver );
 
   delete newdriver.key;
