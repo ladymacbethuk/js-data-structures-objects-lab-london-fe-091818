43eb80ed58aae9879172a62be19065b4f51bb69d
Stephanie Phillips <steph@ladymacbeth.uk>
2018-10-24 19:25:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ function deleteFromDriverByKey(driver, key) {
 }
 
 
-function destructivelyDeleteFromDriverByKey(driver, key) {
+/* function destructivelyDeleteFromDriverByKey(driver, key) {
   delete driver.key;
   return driver;
 }
 
+*/
